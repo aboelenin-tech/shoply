2aa61f8b789c30a470ac0b2c506e3f4669bf7f94
shamsfarid <shamsfarid83@gmail.com>
2026-09-07 12:34:08 +0300

    login and register pages done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,10 +9,10 @@ function Register() {
                         Shoply
                     </div>
                     <div className="d-flex flex-row gap-4 px-5 ">
-                        <NavLink to="/Home" className="nav-link text-white-50" >
+                        <NavLink to="/Home" className="nav-link " >
                             Home
                         </NavLink>
-                        <NavLink to="/login" className="nav-link text-white-50 " >
+                        <NavLink to="/login" className="nav-link  " >
                             Login
                         </NavLink>
                     </div>
@@ -28,28 +28,28 @@ function Register() {
                         <div className="w-100 d-flex flex-row justify-content-evenly my-3">
                             <div className="d-flex flex-column ">
                                 <label htmlFor="Fname" className="text-white">First name</label>
-                                <input type="text" id="Fname" className=" login-input form-control  border border-light bg-transparent border-opacity-10 w-100 " name="Fname"  placeholder="....." />
+                                <input type="text" id="Fname" className=" login-input form-control  w-100 " name="Fname"  placeholder="....." />
                             </div>
                             <div className="d-flex flex-column ">
                                 <label htmlFor="Lname" className="text-white">Last name</label>
-                                <input type="text" id="Lname" className=" login-input form-control  border border-light bg-transparent border-opacity-10 w-100 " name="Lname" placeholder="....." />
+                                <input type="text" id="Lname" className=" login-input form-control  w-100 " name="Lname" placeholder="....." />
                             </div>
                         </div>
 
 
                         <div className="  w-100 d-flex align-items-center flex-column gap-2 ">
                             <label htmlFor="email" name="Email" className="text-white w-75">Email</label>
-                            <input type="email" id="email" className=" login-input form-control w-75 border border-light bg-transparent border-opacity-10 " placeholder="Enter your email" />
+                            <input type="email" id="email" className=" login-input form-control w-75  " placeholder="Enter your email" />
                             <label htmlFor="pass" className="text-white w-75 mt-3">Password</label>
-                            <input type="password" id="pass" name="pass" className=" login-input form-control w-75 bg-transparent border border-light border-opacity-10" placeholder="Enter your password " />
+                            <input type="password" id="pass" name="pass" className=" login-input form-control w-75 " placeholder="Enter your password " />
                             <label htmlFor="Cpass" className="text-white w-75 mt-3">Confirm Password</label>
-                            <input type="password" id="Cpass" name="Cpass" className=" login-input form-control w-75 bg-transparent border border-light border-opacity-10" placeholder="Repeat your password " />
+                            <input type="password" id="Cpass" name="Cpass" className=" login-input form-control w-75 " placeholder="Repeat your password " />
                         </div>
                         <div className="w-75 mt-3">
                             <input type="checkbox" id="agree" className="form-check-input" />
                             <label htmlFor="agree" className="text-white-50 mx-2">i agree to Shoply's <span className="text-primary">Terms of Service</span> and <span className="text-primary">Privacy Policy</span></label>
                         </div>
-                        <button type="submit" className="btn border border-light px-5  border-opacity-10 rounded text-white-50 w-75">submit</button>
+                        <button type="submit" className="btn  px-5 rounded bg-primary w-75">submit</button>
 
                     </form>
 
